8d4ef776b602b36a07def61d5d05c66b8651f91a
saintdmix <josephchukwuebuka10@gmail.com>
2025-02-24 16:34:05 +0000

    Update downloadPage.dart

--- lib/downloadPage.dart
+++ lib/downloadPage.dart
@@ -1,39 +1,33 @@
+
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class PaystackWeb extends StatefulWidget {
-  const PaystackWeb({
-    super.key,
-  });
+class DownloadPage extends StatelessWidget {
+  final String apkUrl =
+      "https://github.com/saintdmix/Nri/releases/latest/download/Nri.apk";
 
-  @override
-  State<PaystackWeb> createState() => _PaystackWebState();
-}
+  Future<void> _downloadApk() async {
+    Uri url = Uri.parse(apkUrl);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(
+        url,
+        mode: LaunchMode.externalApplication, // Opens in Chrome/Safari
+      );
+    } else {
+      throw "Could not launch $apkUrl";
+    }
+  }
 
-class _PaystackWebState extends State<PaystackWeb> {
-  WebViewController controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {
-          // Update loading bar.
-        },
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onHttpError: (HttpResponseError error) {},
-        onWebResourceError: (WebResourceError error) {},
-      ),
-    )
-    ..loadRequest(Uri.parse(
-        'https://github.com/saintdmix/Nri/releases/latest/download/Nri.apk'));
-  bool isLoading = true;
-  // State variable to manage loading state
-  double progress = 0; // Variable to store loading progress
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Download Page')),
-      body: WebViewWidget(controller: controller),
+      appBar: AppBar(title: Text("Download APK")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _downloadApk,
+          child: Text("Download APK"),
+        ),
+      ),
     );
   }
 }
