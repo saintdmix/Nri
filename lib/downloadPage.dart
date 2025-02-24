060ed2e1e57611b7593a841cc53fc1f396d251d0
saintdmix <josephchukwuebuka10@gmail.com>
2025-02-24 15:53:33 +0000

    Update downloadPage.dart

--- lib/downloadPage.dart
+++ lib/downloadPage.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'package:nriweb/constants/constStyles.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class PaystackWeb extends StatefulWidget {
@@ -13,47 +11,29 @@ class PaystackWeb extends StatefulWidget {
 }
 
 class _PaystackWebState extends State<PaystackWeb> {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onHttpError: (HttpResponseError error) {},
+        onWebResourceError: (WebResourceError error) {},
+      ),
+    )
+    ..loadRequest(Uri.parse(
+        'https://github.com/saintdmix/Nri/releases/latest/download/Nri.apk'));
   bool isLoading = true;
   // State variable to manage loading state
   double progress = 0; // Variable to store loading progress
   @override
   Widget build(BuildContext context) {
-    WebViewController controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(
-          "https://github.com/saintdmix/Nri/releases/latest/download/Nri.apk"))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progressValue) {
-            // Update loading progress only when it changes significantly
-            double newProgress = progressValue / 100;
-            if ((newProgress - progress).abs() >= 0.01) {
-              // Only update for significant changes
-              progress = newProgress;
-            }
-          },
-          onPageFinished: (url) {
-            // Perform any navigation or action after the page loads
-            // Navigator.of(context).push(MaterialPageRoute(builder: (_) => VerifyPage()));
-          },
-        ),
-      );
     return Scaffold(
-      appBar: AppBar(
-          title: const Text('Download Page',
-              style: ConstStyles.SofiaProBold18Black)),
-      body: Center(child: Text("Be patient while download begin...")),
+      appBar: AppBar(title: const Text('Download Page')),
+      body: WebViewWidget(controller: controller),
     );
-  }
-
-  void processPayments(List<String> sellerIds) {
-    for (var sellerId in sellerIds) {
-      // Call your payment method for each sellerId
-      sendPaymentToSeller(sellerId);
-    }
-  }
-
-  void sendPaymentToSeller(String sellerId) {
-    // Your payment logic here
   }
 }
